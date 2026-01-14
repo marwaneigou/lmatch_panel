52d5f2605b112983c75968f976d0d50e0cd90f8a
marwaneigou <igourramenmarwane@gmail.com>
2026-01-14 12:49:24 +0100

    Add storage folder

--- storage/framework/views/8c23517b3e5756cabe73f4b478935d1ab8018457.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('content'); ?>
-
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.iactive', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /home/admin/domains/newpanel.kingiptv.pro/project/resources/views/active.blade.php ENDPATH**/ ?>
